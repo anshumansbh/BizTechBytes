2d6d03d52cd7219cf2a0e37bd8337384d7b7f80c
anshumansbh <anshuman.in@outlook.com>
2023-07-31 15:25:47 +0530

    Initial commit
    Initial commit with working news fragment

---
+++ app/src/main/java/com/example/biztechbytes/SliderItems.java
@@ -0,0 +1,13 @@
+package com.example.biztechbytes;
+
+public class SliderItems {
+
+    private String image;
+    public SliderItems(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
